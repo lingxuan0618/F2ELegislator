deff46eb69c23f10613f48ee7f8eba81273c03da
lingxuan <lingxuan0618@gmail.com>
2023-11-08 22:36:01 +0800

    新增header component

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex items-center justify-between px-8 w-full fixed bg-white">
+    <div class="flex">
+      <img src="src/assets/image/logo.png" alt="選舉章" class="pr-6">
+      <div class="flex items-center my-2 bg-black rounded-3xl text-white">
+        <h3 class="px-2">2024 立委</h3>
+        <div class="w-6 h-6 rounded-full bg-mainYellow text-center text-black">3</div>
+        <h3 class="px-2">號參選人</h3>
+      </div>
+    </div>
+    <div class="flex">
+      <h4 class="mx-4 cursor-pointer">候選人主張</h4>
+      <h4 class="mx-4 cursor-pointer">最新活動</h4>
+      <h4 class="mx-4 cursor-pointer">政策議題</h4>
+      <h4 class="mx-4 cursor-pointer">民眾服務信箱</h4>
+      <h4 class="mx-4 cursor-pointer">小額捐款</h4>
+    </div>
+    <div class="flex text-green text-2xl">
+      <font-awesome-icon icon="fa-brands fa-facebook" class="p-2" />
+      <font-awesome-icon icon="fa-brands fa-instagram" class="p-2" />
+      <font-awesome-icon icon="fa-solid fa-share-nodes" class="p-2" />
+    </div>
+  </div>
+</template>
