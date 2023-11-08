6cb648d0317a182e43bdfc259fc236c7b9f85f13
lingxuan <lingxuan0618@gmail.com>
2023-11-08 22:36:39 +0800

    新增候選人主張

---
+++ src/components/Advocate.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="min-h-screen">
+    <div class="flex justify-center">
+      <div>
+        <h1>候選人主張</h1>
+        <h3 class="text-green">我堅信 ! </h3>
+        <h3>藉由推動更完善的貓咪福利和相關政策，</h3>
+        <h3>更是間接地投資於<span class="text-mainYellow">台灣的未來</span></h3>
+        <p>畢竟，民眾的身心健康與工作熱情是推動經濟的核心動力。透過完善的貓咪福利政策，為台灣的 GDP 經濟帶來巨大效益。</p>
+        <p>因此，我期望能在立法院內推進這些政策，確保每一隻貓咪都能得到他們應有的照顧，同時也為台灣的經濟發展助一臂之力。</p>
+        <hr>
+        <h3 class="text-green">護航台灣的幸福經濟，從照顧每一隻貓咪開始。</h3>
+      </div>
+      <img src="src/assets/image/puzzleMeow.png" alt="puzzleMeow">
+    </div>
+  </div>
+</template>
+<style scoped>
+</style>
