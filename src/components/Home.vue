0dc39e7c3071e8f9c4360be234c764e560d0ad4a
lingxuan <lingxuan0618@gmail.com>
2023-11-08 22:36:27 +0800

    新增喵立翰首頁

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-screen pt-16">
+    <div class="flex justify-center items-center">
+      <img src="src/assets/image/meow.png" alt="meow">
+      <div class="flex flex-col items-center tracking-widest">
+        <img src="src/assets/image/logoGreen.png" alt="meow">
+        <h4 class="text-9xl">台灣的明天</h4>
+        <h4 class="text-9xl">喵先鋪路</h4>
+        <h4 class="px-2 border-2">2024立委3號參選人 少年立委 勤政愛民 愛您！！</h4>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+</style>
