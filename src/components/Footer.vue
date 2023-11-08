be1495de6072d66c53612c841e2568dd6c8a51f8
lingxuan <lingxuan0618@gmail.com>
2023-11-08 22:35:39 +0800

    處理footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,27 @@
 <template>
-<div class="p-8 bg-mainYellow">
-
+<div class="p-12 bg-mainYellow">
+  <div class="flex justify-end w-6/12 pr-8">
+    <div class="mr-2 border-2">
+      <div class="flex items-center">
+        <div class="w-1/4 mx-4">
+          <img src="src/assets/image/seal.png" alt="選舉章">
+        </div>
+        <div class="flex items-center w-3/4 p-2 border-l-2">
+          <div class="pl-4">
+            <h4>好</h4>
+            <h4>喵</h4>
+          </div>
+          <h4 class="px-6 font-black text-7xl">3</h4>
+        </div>
+      </div>
+      <div class="p-1 text-xs text-center border-t-2 tracking-widest">點擊我就喵喵叫給你聽</div>
+    </div>
+    <div class="py-4 px-8 border-2 tracking-widest">
+      <h4 class="pb-2 text-4xl font-black">喵立翰</h4>
+      <h4 class="text-xl text-center">少年立委</h4>
+    </div>
+  </div>
+  <div class="w-6/12"></div>
 </div>
 <div class="flex justify-evenly p-8 bg-black text-white">
   <div>
@@ -16,7 +37,8 @@
     <font-awesome-icon icon="fa-brands fa-facebook" class="p-2" />
     <font-awesome-icon icon="fa-brands fa-instagram" class="p-2" />
     <font-awesome-icon icon="fa-brands fa-x-twitter" class="p-2" />
-    <font-awesome-icon icon="fa-solid fa-share-nodes" class="p-2" />
+    <!-- <font-awesome-icon icon="fa-solid fa-arrow-right" class="p-2"/>
+    <font-awesome-icon icon="fa-solid fa-arrow-left" class="p-2"/> -->
   </div>
 </div>
 </template>
