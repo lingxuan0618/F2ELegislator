150b3245e1dec89b8e3a3ff3a8802db593f2c4ad
lingxuan <lingxuan0618@gmail.com>
2023-11-09 21:15:36 +0800

    部屬github page

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/F2ELegislator/'
+    : '/',
 })
