ba1bf90602a01b1879956a1f961433d86fb23d10
lingxuan <lingxuan0618@gmail.com>
2023-11-11 23:13:29 +0800

    新增候選人主張

--- src/components/Advocate.vue
+++ src/components/Advocate.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="min-h-screen">
+  <div class="min-h-screen pt-60">
     <div class="flex justify-center">
-      <div>
-        <h1>候選人主張</h1>
-        <h3 class="text-green">我堅信 ! </h3>
-        <h3>藉由推動更完善的貓咪福利和相關政策，</h3>
-        <h3>更是間接地投資於<span class="text-mainYellow">台灣的未來</span></h3>
-        <p>畢竟，民眾的身心健康與工作熱情是推動經濟的核心動力。透過完善的貓咪福利政策，為台灣的 GDP 經濟帶來巨大效益。</p>
-        <p>因此，我期望能在立法院內推進這些政策，確保每一隻貓咪都能得到他們應有的照顧，同時也為台灣的經濟發展助一臂之力。</p>
-        <hr>
-        <h3 class="text-green">護航台灣的幸福經濟，從照顧每一隻貓咪開始。</h3>
+      <div class="tracking-wider">
+        <div class="relative">
+          <h1 class="text-64 text-mainBlack">候選人主張</h1>
+          <h1 class="absolute text-yellow text-92 font-light tracking-wider" style="top:-65%">ADVOCATE</h1>
+        </div>
+        <h3 class="text-green text-4xl pt-20">我堅信 ! </h3>
+        <h3 class="text-xl pt-8">藉由推動更完善的貓咪福利和相關政策，</h3>
+        <h3 class="text-xl">更是間接地投資於<span class="text-mainYellow text-4xl">台灣的未來</span></h3>
+        <p class="text-sm pt-8">畢竟，民眾的身心健康與工作熱情是推動經濟的核心動力。透過</p>
+        <p class="text-sm">完善的貓咪福利政策，為台灣的 GDP 經濟帶來巨大效益。</p>
+        <p class="text-sm pt-8">因此，我期望能在立法院內推進這些政策，確保每一隻貓咪都能</p>
+        <p class="text-sm pb-4">得到他們應有的照顧，同時也為台灣的經濟發展助一臂之力。</p>
+        <hr class="border-black w-1/2">
+        <h3 class="text-green pt-8">護航台灣的幸福經濟，從照顧每一隻貓咪開始。</h3>
       </div>
-      <img src="src/assets/image/puzzleMeow.png" alt="puzzleMeow">
+      <img src="/src/assets/image/puzzleMeow.png" alt="puzzleMeow">
     </div>
   </div>
 </template>
