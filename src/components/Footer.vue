ace8ed037ffc96217525843c892e5aab87f24ee6
lingxuan <lingxuan0618@gmail.com>
2023-11-11 23:12:52 +0800

    新增跑馬燈

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,29 +1,33 @@
 <template>
-<div class="p-12 bg-mainYellow">
-  <div class="flex justify-end w-6/12 pr-8">
-    <div class="mr-2 border-2">
+<div class="flex p-6 bg-mainYellow">
+  <div class="flex items-center justify-end w-6/12 pr-8">
+    <div class="mr-2 border-2 border-black">
       <div class="flex items-center">
         <div class="w-1/4 mx-4">
-          <img src="src/assets/image/seal.png" alt="選舉章">
+          <img src="/src/assets/image/seal.png" alt="選舉章">
         </div>
-        <div class="flex items-center w-3/4 p-2 border-l-2">
+        <div class="flex items-center w-3/4 p-2 border-l-2 border-black">
           <div class="pl-4">
             <h4>好</h4>
             <h4>喵</h4>
           </div>
-          <h4 class="px-6 font-black text-7xl">3</h4>
+          <h4 class="px-6 text-mainBlack text-7xl">3</h4>
         </div>
       </div>
-      <div class="p-1 text-xs text-center border-t-2 tracking-widest">點擊我就喵喵叫給你聽</div>
+      <div class="p-1 text-xs text-center border-t-2 border-black tracking-widest">點擊我就喵喵叫給你聽</div>
     </div>
-    <div class="py-4 px-8 border-2 tracking-widest">
-      <h4 class="pb-2 text-4xl font-black">喵立翰</h4>
+    <div class="py-5 px-8 border-2 border-black tracking-widest">
+      <h4 class="pb-2 text-4xl text-mainBlack">喵立翰</h4>
       <h4 class="text-xl text-center">少年立委</h4>
     </div>
   </div>
-  <div class="w-6/12"></div>
+  <div class="w-6/12 tracking-wider">
+    <marquee class="text-40 ml-20 font-bold">凍蒜! 凍蒜! <span>TAIWAN NO.1</span>   2024 VOTE NO.3</marquee>
+    <marquee class="text-40 ml-10 font-bold"><span>ELECTIONS</span>少年立委 清流參選 台灣的未來，差您一票</marquee>
+    <marquee class="text-64 ml-16 font-bold">TAIWAN NO.1<span class="pl-4">ELECTIONS 凍蒜！凍蒜！</span></marquee>
+  </div>
 </div>
-<div class="flex justify-evenly p-8 bg-black text-white">
+<div class="flex justify-evenly p-8 bg-mainBlack text-white">
   <div>
     <p class="pb-2 text-2xl">競選總部</p>
     <h4>地址：台灣喵星區</h4>
@@ -42,4 +46,18 @@
   </div>
 </div>
 </template>
+<style lang="scss" scoped>
+  span {
+    text-align:center;
+    color:transparent;
+    -webkit-text-fill-color: 60px #FFBB18;
+    -webkit-text-stroke: 0.002px #000;
+  }
+  h6 {
+    text-align:center;
+    color:transparent;
+    -webkit-text-fill-color: 60px #FFBB18;
+    -webkit-text-stroke: 2px #000;
+  }
+</style>
 
