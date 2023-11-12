6fe464c59aac7a9e1f17f32db65a300df5832b00
lingxuan <lingxuan0618@gmail.com>
2023-11-12 13:49:20 +0800

    加上所有component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <script setup>
+import Header from './components/Header.vue'
+import Home from './components/Home.vue'
+import Advocate from './components/Advocate.vue'
+import News from './components/News.vue'
+import Policy from './components/Policy.vue'
+import SupportUs from './components/SupportUs.vue'
+import ContactUs from './components/ContactUs.vue'
 import Footer from './components/Footer.vue'
+import RotateText from './components/RotateText.vue'
 </script>
 
 <template>
+  <Header></Header>
   <div class="relative">
-    <div class="transform -rotate-90 fixed bottom-80 left-0">
-      <h6>台灣的未來，由您選擇!台灣的未來，由您選擇!</h6>
-      <h6>Miao Li-Han © 2023 Copyright. All Rights Reserved</h6>
+    <div class="fixed bottom-80 left-0">
+    <!-- <RotateText></RotateText> -->
+      <!-- <h6>台灣的未來，由您選擇!台灣的未來，由您選擇!</h6> -->
+      <h6 class="transform -rotate-90">Miao Li-Han © 2023 Copyright. All Rights Reserved</h6>
     </div>
   </div>
+  <Home></Home>
+  <Advocate></Advocate>
+  <News></News>
+  <Policy></Policy>
+  <SupportUs></SupportUs>
+  <ContactUs></ContactUs>
   <Footer></Footer>
 </template>
 
