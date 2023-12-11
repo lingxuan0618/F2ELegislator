78bca7f4d75d8615094ddf48876eba65eb488936
lingxuan <lingxuan0618@gmail.com>
2023-12-11 21:05:26 +0800

    新增文字圍成一個圈(使用svg)以及加上transform一直旋轉的效果

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,31 @@ import Policy from './components/Policy.vue'
 import SupportUs from './components/SupportUs.vue'
 import ContactUs from './components/ContactUs.vue'
 import Footer from './components/Footer.vue'
-import RotateText from './components/RotateText.vue'
 </script>
 
 <template>
   <Header></Header>
   <div class="relative">
     <div class="fixed bottom-80 left-0">
-    <!-- <RotateText></RotateText> -->
-      <!-- <h6>台灣的未來，由您選擇!台灣的未來，由您選擇!</h6> -->
-      <h6 class="transform -rotate-90">Miao Li-Han © 2023 Copyright. All Rights Reserved</h6>
+      <h6 class="relative transform -rotate-90">Miao Li-Han © 2023 Copyright. All Rights Reserved
+        <div class="fixed" style="left: 55%;top: -480%;">
+          <!-- 參考 https://svg-tutorial.com/ -->
+          <svg width="260" height="260" viewBox="-100 -100 200 200" class="rotateText">
+            <defs>
+              <path id="text-arc" d="M 0, 50 A 50 50 0 1 1 1,50" />
+            </defs>
+            <text
+              fill="#000000"
+              letter-spacing="0.25rem"
+              font-size="0.7rem"
+            >
+              <textPath href="#text-arc">
+                台灣的未來˙由您選擇&nbsp; 台灣的未來˙由您選擇&nbsp;
+              </textPath>
+            </text>
+          </svg>
+        </div>
+      </h6>
     </div>
   </div>
   <Home></Home>
@@ -40,5 +55,19 @@ import RotateText from './components/RotateText.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+.rotateText {
+    animation: rotateTextInfinite 10s linear infinite;
+  }
+@keyframes rotateTextInfinite {
+  0%{ 
+    transform: rotate(0deg)
+  }
+  50% {  
+    transform:rotate(180deg);
+  }
+  100% {  
+    transform:rotate(360deg);
+  }
 }
 </style>
